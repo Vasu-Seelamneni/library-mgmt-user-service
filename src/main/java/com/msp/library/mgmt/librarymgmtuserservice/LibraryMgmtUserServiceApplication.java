8614e57a27a788a4373ecd73474dee5a0377f765
Vasu Seelamneni <Vasu_Seelamneni@Dell.com>
2024-11-05 15:56:54 +0800

    Library management user services project

---
+++ src/main/java/com/msp/library/mgmt/librarymgmtuserservice/LibraryMgmtUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.msp.library.mgmt.librarymgmtuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMgmtUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryMgmtUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msp/library/mgmt/librarymgmtuserservice/LibraryMgmtUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msp.library.mgmt.librarymgmtuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMgmtUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
